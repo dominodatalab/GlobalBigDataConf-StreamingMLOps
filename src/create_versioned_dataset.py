92dc7302358384e3bd3be564b2e8fa1d938ca422
wadkars <wadkar.sameer@gmail.com>
2021-10-11 08:52:31 -0400

    Add requirements.tx

--- src/create_versioned_dataset.py
+++ src/create_versioned_dataset.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     for f in files:
         inp_files.append(os.path.join(inf_truth_join_folder,f))
     fldr = model_utils.create_if_not_exists_dataset_next_version_folder()
-    
+
     with open(os.path.join(fldr,model_utils.get_model_metadata()['dataset_file']), 'w') as ds_file:
         with fileinput.input(files=inp_files) as inputs:
             for line in inputs:
