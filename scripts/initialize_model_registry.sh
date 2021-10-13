6eb99e5ea382c4b27ddfcb33db08eca89204edea
Sameer Wadkar <sameer.wadkar@dominodatalab.com>
2021-10-13 05:54:37 -0700

    Pushed from Domino: /u/sameer_wadkar/bigdata-conference-streamingml/runs/6165ca37e1bdfe705350d4e0

--- scripts/initialize_model_registry.sh
+++ scripts/initialize_model_registry.sh
@@ -5,6 +5,6 @@ mkdir /mnt/data/my_model_registry/models
 mkdir /mnt/data/my_model_registry/raw_datasets
 mkdir /mnt/data/my_model_registry/training_datasets
 mkdir /mnt/data/my_model_registry/inf_truth_join_folder
-
+cp /mnt/data/bigdata-conference-streamingml/sameer_wadkar/raw_dataset/creditcard.csv /mnt/data/my_model_registry/raw_datasets/
 
 cp ./config/model_metadata_initial.json /mnt/data/my_model_registry/config/model_metadata.json
