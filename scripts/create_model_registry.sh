60b0c875dd7688688f1abaad7f170e563db7b578
Sameer Wadkar <sameer.wadkar@dominodatalab.com>
2021-10-13 04:07:15 -0700

    Pushed from Domino: /u/sameer_wadkar/bigdata-conference-streamingml/runs/6165ca37e1bdfe705350d4e0

--- scripts/create_model_registry.sh
+++ scripts/create_model_registry.sh
@@ -1,6 +1,6 @@
 mkdir /mnt/data/$(DOMINO_PROJECT_NAME)/$(DOMINO_STARTING_USERNAME)
-mkdir /mnt/data/$(DOMINO_PROJECT_NAME)/$(DOMINO_STARTING_USERNAME)config
-mkdir /mnt/data/$(DOMINO_PROJECT_NAME)/$(DOMINO_STARTING_USERNAME)models
+mkdir /mnt/data/$(DOMINO_PROJECT_NAME)/$(DOMINO_STARTING_USERNAME)/config
+mkdir /mnt/data/$(DOMINO_PROJECT_NAME)/$(DOMINO_STARTING_USERNAME)/models
 mkdir /mnt/data/$(DOMINO_PROJECT_NAME)/$(DOMINO_STARTING_USERNAME)/raw_datasets
 mkdir /mnt/data/$(DOMINO_PROJECT_NAME)/$(DOMINO_STARTING_USERNAME)/training_datasets
 mkdir /mnt/data/$(DOMINO_PROJECT_NAME)/$(DOMINO_STARTING_USERNAME)/inf_truth_join_folder
---
+++ scripts/initialize_model_registry.sh
@@ -0,0 +1,10 @@
+rm -rf /mnt/data/my_model_registry/
+mkdir /mnt/data/my_model_registry/
+mkdir /mnt/data/my_model_registry/config
+mkdir /mnt/data/my_model_registry/models
+mkdir /mnt/data/my_model_registry/raw_datasets
+mkdir /mnt/data/my_model_registry/training_datasets
+mkdir /mnt/data/my_model_registry/inf_truth_join_folder
+
+
+cp ./config/model_metadata_initial.json /mnt/data/my_model_registry/config/model_metadata.json
